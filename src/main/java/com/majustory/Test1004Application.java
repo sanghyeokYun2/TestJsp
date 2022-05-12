5f1433bad80e585d72800f7ca74af62c59ebd67d
sang hyeok <lihan7777@naver.com>
2022-05-12 11:20:55 +0900

    첫번쨰 작업

---
+++ src/main/java/com/majustory/Test1004Application.java
@@ -0,0 +1,13 @@
+package com.majustory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test1004Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test1004Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/majustory/Test1004ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majustory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test1004ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
